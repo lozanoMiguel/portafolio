0948b61e0488d99b1996ee41a37c1524b65314f6
lozanoMiguel <lozanomiguel92@gmail.com>
2023-04-04 01:01:22 +0200

    project start

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <nav>
+            <ul>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Projects</li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
