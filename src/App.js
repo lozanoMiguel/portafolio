58775a43d152198a02b2d8a048fd03e187b01f82
lozanoMiguel <lozanomiguel92@gmail.com>
2023-04-04 02:00:38 +0200

    "working on header"

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Header from './components/Header'
 function App() {
   return (
     <div className="App">
-      <h1>holaaaaa!</h1>
+      <Header />
+    
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import '../styles/Header.css'
 
 export default function Header() {
   return (
-    <header>
-        <nav>
-            <ul>
+    <header id='header'>
+        <h2 className='myName'>Miguel Lozano</h2>
+        <nav className='navbar'>
+            <ul className='navbar_list'>
                 <li>About</li>
                 <li>Contact</li>
                 <li>Projects</li>
