1f6d6c7fac2dbf9ce40a4236c5b13ac7d086025f
lozanoMiguel <lozanomiguel92@gmail.com>
2023-04-10 14:25:51 +0200

    "hook color done"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/Header.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLanguage } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate } from "react-router-dom";
+import useColor from '../hooks/useColor.jsx'
 
 export default function Header() {
 
   const [toggleButton, setToggleButton] = useState(false)
+  const [fun] = useColor()
   
-  //navegamos hacia la pagina que se indique
   const navigate = useNavigate();
-  //obtenemos las variables css para cambiar los colores segun la pagina donde estemos
-  const root = document.querySelector(':root');
   
-  const handleNavigate = (e) =>{
-    if(e === '/'){
-      root.style.setProperty('--primary-color-bg', '#CBE4DE')
-      root.style.setProperty('--secondary-color-bg', '#313b57')
-      root.style.setProperty('--primary-color-text','#CBE4DE')
-      root.style.setProperty('--secondary-color-text', '#313b57')
-      root.style.setProperty('--filter', 'invert(17%) sepia(9%) saturate(3003%) hue-rotate(187deg) brightness(98%) contrast(82%)')
-      root.style.setProperty('--filter1', 'invert(91%) sepia(22%) saturate(140%) hue-rotate(115deg) brightness(95%) contrast(91%)')
-    }else if(e === '/about'){
-      root.style.setProperty('--primary-color-bg', '#40513B')
-      root.style.setProperty('--secondary-color-bg', '#40513B')
-      root.style.setProperty('--primary-color-text','#EDF1D6')
-      root.style.setProperty('--secondary-color-text', '#EDF1D6')
-      root.style.setProperty('--filter', 'invert(91%) sepia(11%) saturate(355%) hue-rotate(33deg) brightness(107%) contrast(89%)')
-      root.style.setProperty('--filter1', 'invert(96%) sepia(6%) saturate(663%) hue-rotate(23deg) brightness(103%) contrast(89%)')
-    }else if(e === '/contact'){
-      root.style.setProperty('--primary-color-bg', '#A9907E')
-      root.style.setProperty('--secondary-color-bg', '#A9907E')
-      root.style.setProperty('--primary-color-text','#F3DEBA')
-      root.style.setProperty('--secondary-color-text', '#F3DEBA')
-      root.style.setProperty('--filter', 'invert(92%) sepia(34%) saturate(433%) hue-rotate(315deg) brightness(99%) contrast(93%)')
-      root.style.setProperty('--filter1', 'invert(96%) sepia(6%) saturate(663%) hue-rotate(23deg) brightness(103%) contrast(89%)')
-    }
-    navigate(`${e}`);
-  }
-  
+  useEffect(()=>{
+    fun();
+  })
+
   const handleToggle = () =>{
     if(toggleButton)
       setToggleButton(false)
@@ -49,7 +26,7 @@ export default function Header() {
   return (
     <header id='header' className= { toggleButton ? 'active' : '' }>
         <div className='brand'>
-          <a onClick={()=>{handleNavigate('/')}}>Miguel Lozano</a>
+          <a onClick={()=>navigate('/')}>Miguel Lozano</a>
         </div>
          <div className={toggleButton ? 'translate active' : 'translate'}>
           <FontAwesomeIcon icon={faLanguage} className='translate_icon' />
@@ -61,8 +38,8 @@ export default function Header() {
             <span className='bar'></span>
           </a>
           <ul className={ toggleButton ? 'navbar_list active' : 'navbar_list' }>
-            <li onClick={()=>{handleNavigate('/about')}}>About</li>
-            <li onClick={()=>{handleNavigate('/contact')}}>Contact</li>
+            <li onClick={()=>navigate('/about')}>About</li>
+            <li onClick={()=>navigate('/contact')}>Contact</li>
             <li>Projects</li>
           </ul>
         </nav>
---
+++ src/hooks/useColor.jsx
@@ -0,0 +1,37 @@
+
+import { useLocation } from "react-router-dom";
+
+export default function useColor() {
+
+  const location = useLocation();
+  const root = document.querySelector(':root');
+
+  const colorChange = () =>{
+    if(location.pathname === '/'){
+      root.style.setProperty('--primary-color-bg', '#CBE4DE')
+      root.style.setProperty('--secondary-color-bg', '#313b57')
+      root.style.setProperty('--primary-color-text','#CBE4DE')
+      root.style.setProperty('--secondary-color-text', '#313b57')
+      root.style.setProperty('--filter', 'invert(17%) sepia(9%) saturate(3003%) hue-rotate(187deg) brightness(98%) contrast(82%)')
+      root.style.setProperty('--filter1', 'invert(91%) sepia(22%) saturate(140%) hue-rotate(115deg) brightness(95%) contrast(91%)')
+    }else if(location.pathname === '/about'){
+      root.style.setProperty('--primary-color-bg', '#40513B')
+      root.style.setProperty('--secondary-color-bg', '#40513B')
+      root.style.setProperty('--primary-color-text','#EDF1D6')
+      root.style.setProperty('--secondary-color-text', '#EDF1D6')
+      root.style.setProperty('--filter', 'invert(91%) sepia(11%) saturate(355%) hue-rotate(33deg) brightness(107%) contrast(89%)')
+      root.style.setProperty('--filter1', 'invert(96%) sepia(6%) saturate(663%) hue-rotate(23deg) brightness(103%) contrast(89%)')
+    }else if(location.pathname === '/contact'){
+      root.style.setProperty('--primary-color-bg', '#A9907E')
+      root.style.setProperty('--secondary-color-bg', '#A9907E')
+      root.style.setProperty('--primary-color-text','#F3DEBA')
+      root.style.setProperty('--secondary-color-text', '#F3DEBA')
+      root.style.setProperty('--filter', 'invert(92%) sepia(34%) saturate(433%) hue-rotate(315deg) brightness(99%) contrast(93%)')
+      root.style.setProperty('--filter1', 'invert(96%) sepia(6%) saturate(663%) hue-rotate(23deg) brightness(103%) contrast(89%)')
+    }
+  }
+
+  return (
+   [colorChange]
+  )
+}
