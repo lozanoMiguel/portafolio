a7bb0faeefc0c4cde5dc5179729458f1838f198a
lozanoMiguel <lozanomiguel92@gmail.com>
2023-05-03 10:20:27 +0200

    "a few steps to finish"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Footer.css'
 import mail from '../images/mail.svg'
 import instagram from '../images/brand-instagram.svg'
 import linkedin from '../images/brand-linkedin.svg'
+import { useLocation } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
 
 export default function Footer() {
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
   return (
     <footer id='footer' className='footer'>
       <div className='footer_info'>
         <div className='footer_info-message'>
           <h2>Let's go work togheter?</h2>
-          <p>Let's get in touch! <span><a href="#">Contact</a></span> </p>
+          <p className={ location.pathname === '/contact' ? 'hiddenP' : ''}>Let's get in touch! <span><a onClick={()=>navigate('/contact')}>Contact</a></span> </p>
         </div>
       </div>
       <div className='footer_socialMedia'>
@@ -19,13 +25,13 @@ export default function Footer() {
           <p>My social media</p>
         </div>
         <div className='footer_socialMedia-icons'>
-          <a href="#">
+          <a href="https://www.instagram.com/_lozanomiguel">
             <img src={ instagram } alt="@_lozanomiguel" className='icon filter'/>
           </a>
           <a href="https://linkedin.com/in/lozanomiguel">
             <img src={ linkedin } alt="https://linkedin.com/in/lozanomiguel" className='icon filter'/>
           </a>
-          <a href="#">
+          <a href="mailto:lozanomiguel92@gmail.com">
             <img src={ mail } alt="lozanomiguel92@gmail.com" className='icon filter'/>
           </a>
         </div>
--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import '../styles/ProjectsContainer.css'
 import Project from './Project'
 import p1 from '../images/equipoHistorico.png'
+import p2 from '../images/libroNovedades.png'
+import p3 from '../images/notes.png'
+import p4 from '../images/batSignal.png'
 
 export default function ProjectsContainer() {
   return (
@@ -17,8 +20,18 @@ export default function ProjectsContainer() {
             description = {'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat molestiae qui debitis assumenda ipsam obcaecati odit mollitia repellendus. Eum, dicta doloremque! Dolor quisquam nemo beatae quo expedita consequuntur illum aspernatur?'}
           />
           <Project 
-            img = { p1 }
-            title = {'Equipo histórico'}
+            img = { p2 }
+            title = {'Libro novedades'}
+            description = {'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat molestiae qui debitis assumenda ipsam obcaecati odit mollitia repellendus. Eum, dicta doloremque! Dolor quisquam nemo beatae quo expedita consequuntur illum aspernatur?'}
+          />
+          <Project 
+            img = { p3 }
+            title = {'Notas'}
+            description = {'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat molestiae qui debitis assumenda ipsam obcaecati odit mollitia repellendus. Eum, dicta doloremque! Dolor quisquam nemo beatae quo expedita consequuntur illum aspernatur?'}
+          />
+          <Project 
+            img = { p4 }
+            title = {'Bati señal'}
             description = {'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat molestiae qui debitis assumenda ipsam obcaecati odit mollitia repellendus. Eum, dicta doloremque! Dolor quisquam nemo beatae quo expedita consequuntur illum aspernatur?'}
           />
         </div>
