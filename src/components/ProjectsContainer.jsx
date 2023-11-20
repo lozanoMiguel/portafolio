1f0542e4f9b75dd3d4ac0d068da9a6d5aec9615c
lozanoMiguel <lozanomiguel92@gmail.com>
2023-11-21 00:20:57 +0100

    edit-projects

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,74 +1,67 @@
-import React from 'react'
-import '../styles/ProjectsContainer.css'
-import Project from './Project'
-import p1 from '../images/equipoHistorico.png'
-import p2 from '../images/libroNovedades.png'
-import p3 from '../images/notes.png'
-import p4 from '../images/batSignal.png'
-import p5 from '../images/multirivel.png'
-import p6 from '../images/paola.png'
-import p7 from '../images/lozanomiguel.png'
-import { useTranslation } from 'react-i18next'
-import ModalTransition from './ModalTransition';
+import React from "react";
+import "../styles/ProjectsContainer.css";
+import Project from "./Project";
+import p1 from "../images/equipoHistorico.png";
+import p2 from "../images/libroNovedades.png";
+import p3 from "../images/notes.png";
+import p4 from "../images/batSignal.png";
+import p5 from "../images/multirivel.png";
+import p6 from "../images/paola.png";
+import p7 from "../images/lozanomiguel.png";
+import { useTranslation } from "react-i18next";
+import ModalTransition from "./ModalTransition";
 
 export default function ProjectsContainer() {
+  const [t, i18n] = useTranslation("global");
 
-  const[t, i18n] = useTranslation("global");
-  
   return (
     <>
-      {<ModalTransition sty={'modalTransition on'}/>}
-      <main id='main'>
-        <section className='projectsContainer'>
-          <div className='title'>
-              <h2>{t("projects.projectsTitle")}</h2>
+      {<ModalTransition sty={"modalTransition on"} />}
+      <main id="main">
+        <section className="projectsContainer">
+          <div className="title">
+            <h2>{t("projects.projectsTitle")}</h2>
           </div>
-          <div className='container'>
-            <Project 
-              url = { "https://lozanomiguel.github.io/libroNovedades/" } 
-              img = { p2 }
-              title = {t("projects.p2title")}
-              description = {t("projects.p2")}
-            />
-            <Project 
-              url = { "https://paolakremer.com" } 
-              img = { p6 }
-              title = {t("projects.p6title")}
-              description = {t("projects.p6")}
-            />
-            <Project 
-              url = { "https://github.com/lozanoMiguel/multirivelChallenge" } 
-              img = { p5 }
-              title = {t("projects.p5title")}
-              description = {t("projects.p5")}
-            />
-            <Project 
-              url = { "https://lozanomiguel.github.io/batSignal/" } 
-              img = { p4 }
-              title = {t("projects.p4title")}
-              description = {t("projects.p4")}
+          <div className="container">
+            <Project
+              url={"https://lozanomiguel.github.io/libroNovedades/"}
+              img={p2}
+              title={t("projects.p2title")}
+              description={t("projects.p2")}
             />
             <Project
-              url = { "https://lozanomiguel.github.io/equipoHistorico/" } 
-              img = { p1 }
-              title = {t("projects.p1title")}
-              description = {t("projects.p1")}
+              url={"https://paolakremer.com"}
+              img={p6}
+              title={t("projects.p6title")}
+              description={t("projects.p6")}
             />
-            <Project 
-              url = { "https://lozanomiguel.github.io/notes/" } 
-              img = { p3 }
-              title = {t("projects.p3title")}
-              description = {t("projects.p3")}
+            <Project
+              url={"https://github.com/lozanoMiguel/multirivelChallenge"}
+              img={p5}
+              title={t("projects.p5title")}
+              description={t("projects.p5")}
             />
-            <Project 
-              url = { "https://github.com/lozanoMiguel/portafolio" } 
-              img = { p7 }
-              title = {t("projects.p7title")}
-              description = {t("projects.p7")}
+            <Project
+              url={"https://lozanomiguel.github.io/batSignal/"}
+              img={p4}
+              title={t("projects.p4title")}
+              description={t("projects.p4")}
+            />
+            <Project
+              url={"https://lozanomiguel.github.io/equipoHistorico/"}
+              img={p1}
+              title={t("projects.p1title")}
+              description={t("projects.p1")}
+            />
+            <Project
+              url={"https://lozanomiguel.github.io/notes/"}
+              img={p3}
+              title={t("projects.p3title")}
+              description={t("projects.p3")}
             />
           </div>
         </section>
       </main>
     </>
-  )
+  );
 }
