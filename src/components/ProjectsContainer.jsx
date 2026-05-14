deb78e62927d4c328e93c138679793279b3415f5
miguel <lozanomiguel92@gmail.com>
2026-05-14 13:52:48 +0200

    quitando proyectos obsoletos

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 import "../styles/ProjectsContainer.css";
 import Project from "./Project";
-import p1 from "../images/equipoHistorico.png";
 import p2 from "../images/libroNovedades.png";
-import p3 from "../images/notes.png";
 import p4 from "../images/batSignal.png";
-import p5 from "../images/multirivel.png";
-import p6 from "../images/paola.png";
-import p7 from "../images/lozanomiguel.png";
 import { useTranslation } from "react-i18next";
 import ModalTransition from "./ModalTransition";
 
@@ -30,34 +25,10 @@ export default function ProjectsContainer() {
               description={t("projects.p2")}
             />
             <Project
-              url={"https://paolakremer.com"}
-              img={p6}
-              title={t("projects.p6title")}
-              description={t("projects.p6")}
-            />
-            <Project
-              url={"https://github.com/lozanoMiguel/multirivelChallenge"}
-              img={p5}
-              title={t("projects.p5title")}
-              description={t("projects.p5")}
-            />
-            <Project
               url={"https://lozanomiguel.github.io/batSignal/"}
               img={p4}
               title={t("projects.p4title")}
               description={t("projects.p4")}
-            />
-            <Project
-              url={"https://lozanomiguel.github.io/equipoHistorico/"}
-              img={p1}
-              title={t("projects.p1title")}
-              description={t("projects.p1")}
-            />
-            <Project
-              url={"https://lozanomiguel.github.io/notes/"}
-              img={p3}
-              title={t("projects.p3title")}
-              description={t("projects.p3")}
             />
           </div>
         </section>
